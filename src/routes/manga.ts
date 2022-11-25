7fb2e03f6997d6141c84d62a288f5378e246ce72
Daniel Sanchez <danxschz@gmail.com>
2022-11-25 02:21:45 -0400

    Update manga detail function

--- src/routes/manga.ts
+++ src/routes/manga.ts
@@ -20,44 +20,74 @@ function list(req: Request, res: Response) {
 }
 
 // Manga detail
-const query = `
-  SELECT
-    m.id,
-    m.slug,
-    m.title,
-    m.img_normal,
-    m.img_large,
-    m.synopsis,
-    m.background,
-    m.chapters,
-    m.volumes,
-    s.name as status,
-    m.published,
-    r.name as rating,
-    m.links_amazon,
-    m.links_viz,
-    m.links_cdjapan,
-    m.links_mal,
-    m.links_wikipedia
-  FROM
-    manga m
-  INNER JOIN
-    status s
-  ON
-    m.status_id = s.id
-  LEFT JOIN
-    ratings r
-  ON 
-    m.rating_id = r.id
-  WHERE
-    m.id = $1
-`;
-
 async function detail(req: Request, res: Response, next: NextFunction) {
+  const query = `
+    SELECT
+      m.id,
+      m.slug,
+      m.title,
+      m.img_normal,
+      m.img_large,
+      m.synopsis,
+      m.background,
+      m.chapters,
+      m.volumes,
+      s.name AS status,
+      m.published,
+      r.name AS rating,
+      m.links_amazon,
+      m.links_viz,
+      m.links_cdjapan,
+      m.links_mal,
+      m.links_wikipedia, 
+      (
+        SELECT string_agg("title", ', ')
+        FROM manga_alt_titles
+        WHERE manga_id = m.id
+      ) AS alt_titles
+    FROM manga m
+    INNER JOIN status s
+    ON m.status_id = s.id
+    LEFT JOIN ratings r
+    ON m.rating_id = r.id
+    WHERE m.id = $1
+  `;
+
   try {
-    const manga: unknown = await db.any(query, [119021]);
-    return res.send(manga);
-  } 
+    const manga: any = await db.one(query, [119021]);
+    
+    const {
+      img_normal, 
+      img_large, 
+      alt_titles, 
+      links_amazon, 
+      links_viz, 
+      links_cdjapan, 
+      links_mal, 
+      links_wikipedia, 
+      ...result
+    } = manga;
+
+    const img = {
+      normal: img_normal,
+      large: img_large,
+    };
+    result.img = img;
+
+    const titlesArray = alt_titles.split(', ');
+    result.alt_titles = titlesArray;
+
+    const links = {
+      amazon: links_amazon,
+      viz: links_viz,
+      cdjapan: links_cdjapan,
+      mal: links_mal,
+      wikipedia: links_wikipedia,
+    };
+    result.links = links;
+    
+    return res.send(result);
+  }
   catch(e) {
     next(e);
   }
